01424462a890e6d7474dc146ec2c53c298b84aad
mdouglasfernandes <mdouglasfernandes@gmail.com>
2025-09-20 23:02:13 +0000

    Add nascidos-2025.js

---
+++ nascidos-2025.js
@@ -0,0 +1,48 @@
+let nascimento = {
+    "2025": {
+        "JAN-MAR": "Berçário 1"
+    },
+    "2024": {
+        "JAN-MAR": "Berçário 2",
+        "ABR-DEZ": "Berçário 1"
+    },
+    "2023": {
+        "JAN-MAR": "Maternal 1",
+        "ABR-DEZ": "Berçário 2"
+    },
+    "2022": {
+        "JAN-MAR": "Maternal 2",
+        "ABR-DEZ": "Maternal 1"
+    },
+    "2021": {
+        "JAN-MAR": "Jardim 1",
+        "ABR-DEZ": "Maternal 2"
+    },
+    "2020": {
+        "JAN-MAR": "Jardim 2",
+        "ABR-DEZ": "Jardim 1"
+    },
+    "2019": {
+        "JAN-MAR": "1° ano",
+        "ABR-DEZ": "Jardim 2"
+    },
+    "2018": {
+        "JAN-MAR": "2° ano",
+        "ABR-DEZ": "1° ano"
+    },
+    "2017": {
+        "JAN-MAR": "3° ano",
+        "ABR-DEZ": "2° ano"
+    },
+    "2016": {
+        "JAN-MAR": "4° ano",
+        "ABR-DEZ": "3° ano"
+    },
+    "2015": {
+        "JAN-MAR": "5° ano",
+        "ABR-DEZ": "4° ano"
+    },
+    "2014": {
+        "ABR-DEZ": "5° ano"
+    }
+}
